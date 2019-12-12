da04d692b9446a7a558f19ef2f6b9e0ca5cf179c
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2019-12-12 14:25:43 +0100

    Adding cecond model to view nad working with css

--- mountain_weather/zakopane_weather/views.py
+++ mountain_weather/zakopane_weather/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
-from zakopane_weather.models import DailyForecast,Day
+from zakopane_weather.models import DailyForecast,Day, HourlyForecast
 
 # Create your views here.
 
-class IndexView(ListView):
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-    model = DailyForecast
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['DailyForecast'] = DailyForecast.objects.all()
+        context['HourlyForecast'] = HourlyForecast.objects.all()
+        return context
+    
+    
