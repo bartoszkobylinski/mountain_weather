368bc414ae4bad1d87fd802977ace334c8dc2317
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2020-06-24 13:08:55 +0200

    Adding metods to extract geotags from picture and save to the model

--- mountain_weather/users_app/models.py
+++ mountain_weather/users_app/models.py
@@ -1,3 +1,7 @@
+from PIL import Image 
+from PIL.ExifTags import TAGS
+from PIL.ExifTags import GPSTAGS
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -16,9 +20,65 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=250)
     image = models.ImageField(upload_to='place_picture', blank=True)
+    lat = models.DecimalField(default= 0.0, decimal_places=6, max_digits=10)
+    lon = models.DecimalField(default=0.0, decimal_places=6, max_digits=10)
+
 
     def __str__(self):
         return f" Post published {self.date} by {self.user.username}"
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk })
+
+    def get_exif(self, image):
+        image = Image.open(self.image)
+        image.verify()
+        return image._getexif()
+
+    def get_geotagging(self, exif):
+        if not exif:
+            raise ValueError("No EXIF metadata found")
+
+        geotagging = {}
+        for (idx, tag) in TAGS.items():
+            if tag == 'GPSInfo':
+                if idx not in exif:
+                    raise ValueError("No EXIF geotagging found")
+
+                for (key, val) in GPSTAGS.items():
+                    if key in exif[idx]:
+                        geotagging[val] = exif[idx][key]
+
+        return geotagging
+    
+    def get_decimal_from_dms(self, dms, ref):
+
+        degrees = dms[0][0] / dms[0][1]
+        minutes = dms[1][0] / dms[1][1] / 60.0
+        seconds = dms[2][0] / dms[2][1] / 3600.0
+
+        if ref in ['S', 'W']:
+            degrees = -degrees
+            minutes = -minutes
+            seconds = -seconds
+
+        return round(degrees + minutes + seconds, 5)
+
+    def get_coordinates(self, geotags):
+        lat = self.get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+        lon = self.get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+        return (lat,lon)
+
+
+    def save(self, *args, **kwargs):
+        exif = self.get_exif(self.image)
+        geotags = self.get_geotagging(exif)
+        coordinates =  self.get_coordinates(geotags)
+        lat = coordinates[0]
+        lon = coordinates[1]
+        self.lat = lat
+        self.lon = lon
+        super(Post, self).save(*args, **kwargs)
+        print(f"And that is: {lat} and lon: {lon} ")
